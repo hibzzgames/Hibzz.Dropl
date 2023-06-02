d74e181d5ecc5868973a8bb8824804c61e66f1c9
Hibnu Hishath <hibnuhishath1@gmail.com>
2023-06-02 12:51:34 +0530

    Fixed a consistency issue where the default operations weren't in the right namespace

--- Scripts/Default Operations/TranslateOperation.cs
+++ Scripts/Default Operations/TranslateOperation.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Hibzz.Dropl
+namespace Hibzz.Dropl.DefaultOperations
 {
     public class TranslateOperation : PropertyOperation<Vector3>
     {
--- Scripts/Default Operations/UITintOperation.cs
+++ Scripts/Default Operations/UITintOperation.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Hibzz.Dropl
+namespace Hibzz.Dropl.DefaultOperations
 {
     public class UITintOperation : PropertyOperation<Color>
     {
